c84100d465e0dcbde6add449933eb7391a5e2051
collins-mwangi1 <collins.mwangi1@student.moringaschool.com>
2024-03-28 17:51:58 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
+
+//generate a random intager greater than 0//
+let random = Math.round(Math.random) * 10
+console.log(random)
+
+
+let num3 = 16;
+let num4 = 12;
+let mod = num3 % num4;
+
+console.log(mod);
+
+let num = [20,18,17,15,11,7,1]
+const max = Math.max( 20,18,17,15,11,7,1)
+console.log(max);
